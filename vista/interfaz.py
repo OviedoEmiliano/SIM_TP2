89dea639cece1034f226a8586bcc39092bbe3843
Santiago <santigs211@gmail.com>
2025-05-13 23:44:31 -0300

    vista refactorizada

--- vista/interfaz.py
+++ vista/interfaz.py
@@ -25,7 +25,15 @@ class InterfazGenerador(tk.Tk):
         self._configurar_grid()
         self._mostrar_parametros(2)
 
+    """
 
+
+    
+        FUNCIONES PARA VER LA VENTANA PRINCIPAL
+
+    
+        
+    """
     # Seccion donde se carga la cantidad de numeros a generar.
     def _seccion_cantidad_numeros(self):
         self.cantidad_label = tk.Label(self, text="Cantidad de números a generar:")
@@ -84,7 +92,7 @@ class InterfazGenerador(tk.Tk):
         self.alpha_label = tk.Label(self.prueba_de_bondad, text="Alfa:")
         self.alpha_entry = tk.Entry(self.prueba_de_bondad)
 
-    # Seccion donde se seleccionan los intervalos y generan los graficos
+    # Seccion donde se seleccionan los intervalos y generan los graficos.
     def _seccion_generar_mostrar_graficos(self):
         self.intervalos_label = tk.Label(
             self, text="Número de intervalos:"
@@ -107,6 +115,7 @@ class InterfazGenerador(tk.Tk):
             command=self.controlador.generar_y_mostrar_histograma,
         )
 
+    # Seccion donde se muestran los nros generados y paginados.
     def _seccion_mostrar_nros_generados_paginados(self):
         self.scrollbar = tk.Scrollbar(self)
         self.resultado_text = tk.Text(
@@ -130,18 +139,28 @@ class InterfazGenerador(tk.Tk):
         self._seccion_mostrar_nros_generados_paginados();
         
 
-    # Funcion para configurar el grid de la ventana principal y ordenar los elementos.
-    def _configurar_grid(self):
+
+    """
+    
+
+
+        FUNCIONES PARA ORGANIZAR LA VENTANA PRINCIPAL (GRID)
+
+        
+
+    """
+
+    def _grid_cantidad_numeros(self):
         self.cantidad_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
         self.cantidad_entry.grid(row=0, column=1, padx=5, pady=5, sticky="ew")
-
+    def _grid_seleccion_distribucion(self):
         self.distribucion_frame.grid(
             row=1, column=0, columnspan=2, padx=5, pady=5, sticky="ew"
         )
         self.rb_exponencial.grid(row=0, column=0, padx=5, pady=2, sticky="w")
         self.rb_normal.grid(row=1, column=0, padx=5, pady=2, sticky="w")
         self.rb_uniforme.grid(row=2, column=0, padx=5, pady=2, sticky="w")
-
+    def _grid_parametros(self):
         self.parametros_frame.grid(
             row=2, column=0, columnspan=2, padx=5, pady=5, sticky="ew"
         )
@@ -149,36 +168,49 @@ class InterfazGenerador(tk.Tk):
         self.parametro1_entry.grid(row=0, column=1, padx=5, pady=2, sticky="ew")
         self.parametro2_label.grid(row=1, column=0, padx=5, pady=2, sticky="w")
         self.parametro2_entry.grid(row=1, column=1, padx=5, pady=2, sticky="ew")
-
-
-
-
+    def _grid_pruebas_bondad(self):
         self.prueba_de_bondad.grid(row=3, column=0, columnspan=2, padx=5, pady=5, sticky="ew")
 
         self.prueba_chi_cuadrado.grid(row=0, column=0, padx=5, pady=2, sticky="w")
         self.prueba_ks.grid(row=1, column=0, padx=5, pady=2, sticky="w")
 
-        self.alpha_label.grid(row=2, column=0, padx=5, pady=5, sticky="w")
-        self.alpha_entry.grid(row=2, column=1, padx=5, pady=5, sticky="ew")
-
-
+        self.alpha_label.grid(row=2, column=0, padx=(0, 2), pady=5, sticky="e")
+        self.alpha_entry.grid(row=2, column=1, padx=(2, 0), pady=5, sticky="w")
+    def _grid_generar_mostrar_graficos(self):
         self.intervalos_label.grid(row=4, column=0, padx=5, pady=5, sticky="w")
         self.intervalos_combo.grid(row=4, column=1, padx=5, pady=5, sticky="ew")
-
-
         self.generar_mostrar_boton.grid(
-            row=6, column=0, columnspan=2, padx=5, pady=10, sticky="ew"
+            row=7, column=0, columnspan=2, padx=5, pady=10, sticky="ew"
         )
-
+    def _grid_mostrar_nros_generados_paginados(self):
         self.scrollbar.grid(row=5, column=2, padx=0, pady=5, sticky="ns")
         self.resultado_text.grid(
             row=5, column=0, columnspan=2, padx=5, pady=5, sticky="nsew"
         )
-        self.boton_anterior.grid(row=7, column=0, sticky="ew", padx=5, pady=5)
-        self.boton_siguiente.grid(row=7, column=1, sticky="ew", padx=5, pady=5)
-
+        self.boton_anterior.grid(row=6, column=0, sticky="ew", padx=5, pady=5)
+        self.boton_siguiente.grid(row=6, column=1, sticky="ew", padx=5, pady=5)
+    # Funcion para configurar el grid de la ventana principal y ordenar los elementos.
+    def _configurar_grid(self):
+        self._grid_cantidad_numeros();
+        self._grid_seleccion_distribucion();
+        self._grid_parametros();
+        self._grid_pruebas_bondad();
+        self._grid_generar_mostrar_graficos()
+        self._grid_mostrar_nros_generados_paginados();
+        self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(5, weight=1)
+
+
+    """
+
+    
+
+    FUNCIONES QUE SON UTILIZADAS DENTRO DE LAS SECCIONES
+
+    
+
+    """
 
     # Funcion para mostrar los campos de los parametros de la distribucion seleccionada
     def _mostrar_parametros(self, num_params):
@@ -205,38 +237,31 @@ class InterfazGenerador(tk.Tk):
             self.parametro2_label.grid(row=1, column=0, padx=5, pady=2, sticky="w")
             self.parametro2_entry.grid(row=1, column=1, padx=5, pady=2, sticky="ew")
 
-    # Funcion para mostrar el histograma en una ventana nueva
-    def mostrar_histograma(self, numeros, distribucion, num_intervalos):
-        global ventana_histograma_activa
+        # Funcion para calcular la Anterior Pagina
+    
+    # Funcion para calcular la Pagina Anterior
+    def _anterior_pagina(self):
+        if self.pagina_actual > 0:
+            self.pagina_actual -= 1
+            self.mostrar_pagina_resultados()
 
-        if ventana_histograma_activa:
-            ventana_histograma_activa.destroy()
+    # Funcion para calcular la Siguiente Pagina
+    def _siguiente_pagina(self):
+        total_paginas = math.ceil(len(self.numeros_generados) / self.tamano_pagina)
+        if self.pagina_actual < total_paginas - 1:
+            self.pagina_actual += 1
+            self.mostrar_pagina_resultados()
 
-        histograma_ventana = tk.Toplevel(self)
-        histograma_ventana.title(
-            f"Histograma ({distribucion}, {num_intervalos} intervalos)"
-        )
-        ventana_histograma_activa = histograma_ventana
 
-        fig, ax = plt.subplots()
-        n, bins, patches = ax.hist(numeros, bins=num_intervalos, edgecolor="black")
+    """
+    
 
-        # Creamos las etiquetas personalizadas
-        labels = [f"{bins[0]:.4f}"] + [f"{b:.4f}" for b in bins[1:]]
 
-        # Establecemos las ubicaciones de las etiquetas en los límites de los intervalos
-        ax.set_xticks(bins)
-        ax.set_xticklabels(labels, rotation=45, ha="right")
+    FUNCIONES UTILIZADAS PARA OBTENER LOS VALORES INGRESADOS EN CADA SECCION
 
-        ax.set_xlabel("Intervalos")
-        ax.set_ylabel("Frecuencia")
-        ax.set_title(f"Distribución {distribucion} ({num_intervalos} intervalos)")
-        fig.tight_layout()
+    
 
-        canvas = FigureCanvasTkAgg(fig, master=histograma_ventana)
-        canvas_widget = canvas.get_tk_widget()
-        canvas_widget.pack(fill=tk.BOTH, expand=True)
-        canvas.draw()
+    """
 
     # Funcion para obtener la cantidad de numeros a generar
     def obtener_cantidad(self):
@@ -292,6 +317,17 @@ class InterfazGenerador(tk.Tk):
     def obtener_num_intervalos(self):
         return int(self.intervalos_var.get())
     
+
+    """
+    
+
+
+    FUNCIONES USADAS EN EL CONTROLADOR
+
+    
+    
+    """
+
     # Funcion para mostrar los numeros generados en el cuadro de texto
     def mostrar_resultado(self, texto):
         self.resultado_text.delete(1.0, tk.END)
@@ -317,18 +353,38 @@ class InterfazGenerador(tk.Tk):
 
         self.mostrar_resultado(texto)
 
-    # Funcion para calcular la Anterior Pagina
-    def _anterior_pagina(self):
-        if self.pagina_actual > 0:
-            self.pagina_actual -= 1
-            self.mostrar_pagina_resultados()
+    # Funcion para mostrar el histograma en una ventana nueva
+    def mostrar_histograma(self, numeros, distribucion, num_intervalos):
+        global ventana_histograma_activa
 
-    # Funcion para calcular la Siguiente Pagina
-    def _siguiente_pagina(self):
-        total_paginas = math.ceil(len(self.numeros_generados) / self.tamano_pagina)
-        if self.pagina_actual < total_paginas - 1:
-            self.pagina_actual += 1
-            self.mostrar_pagina_resultados()
+        if ventana_histograma_activa:
+            ventana_histograma_activa.destroy()
+
+        histograma_ventana = tk.Toplevel(self)
+        histograma_ventana.title(
+            f"Histograma ({distribucion}, {num_intervalos} intervalos)"
+        )
+        ventana_histograma_activa = histograma_ventana
+
+        fig, ax = plt.subplots()
+        n, bins, patches = ax.hist(numeros, bins=num_intervalos, edgecolor="black")
+
+        # Creamos las etiquetas personalizadas
+        labels = [f"{bins[0]:.4f}"] + [f"{b:.4f}" for b in bins[1:]]
+
+        # Establecemos las ubicaciones de las etiquetas en los límites de los intervalos
+        ax.set_xticks(bins)
+        ax.set_xticklabels(labels, rotation=45, ha="right")
+
+        ax.set_xlabel("Intervalos")
+        ax.set_ylabel("Frecuencia")
+        ax.set_title(f"Distribución {distribucion} ({num_intervalos} intervalos)")
+        fig.tight_layout()
+
+        canvas = FigureCanvasTkAgg(fig, master=histograma_ventana)
+        canvas_widget = canvas.get_tk_widget()
+        canvas_widget.pack(fill=tk.BOTH, expand=True)
+        canvas.draw()
 
     # Funcion para mostrar la Tabla de frecuencias en el cuadro de texto
     def crear_ventana_tabla_frecuencias(self, tabla):
